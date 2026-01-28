a3355869bd82cc394737f6634bf0fbde8bf01890
Chris Mendoza <chrisqm@amazon.com>
2026-01-28 08:26:15 -0500

    Add resource lsp requests and data types

---
+++ plugins/toolkit/jetbrains-core/src/software/aws/toolkits/jetbrains/services/cfnlsp/protocol/ResourceProtocol.kt
@@ -0,0 +1,81 @@
+// Copyright 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.jetbrains.services.cfnlsp.protocol
+
+import org.eclipse.lsp4j.CompletionItem
+import org.eclipse.lsp4j.TextDocumentIdentifier
+
+// Resource Types
+data class ResourceTypesResult(
+    val resourceTypes: List<String>
+)
+
+// Resource Listing
+data class ResourceRequest(
+    val resourceType: String,
+    val nextToken: String? = null
+)
+
+data class ListResourcesParams(
+    val resources: List<ResourceRequest>? = null
+)
+
+data class ResourceSummary(
+    val typeName: String,
+    val resourceIdentifiers: List<String>,
+    val nextToken: String? = null
+)
+
+data class ListResourcesResult(
+    val resources: List<ResourceSummary>
+)
+
+// TODO: Resource State Operations
+enum class ResourceStatePurpose {
+    IMPORT,
+    CLONE
+}
+
+data class ResourceSelection(
+    val resourceType: String,
+    val resourceIdentifiers: List<String>
+)
+
+data class ResourceStateParams(
+    val textDocument: TextDocumentIdentifier,
+    val resourceSelections: List<ResourceSelection>? = null,
+    val purpose: ResourceStatePurpose,
+    val parentResourceType: String? = null
+)
+
+data class ResourceStateResult(
+    val completionItem: CompletionItem? = null,
+    val successfulImports: Map<String, List<String>>,
+    val failedImports: Map<String, List<String>>,
+    val warning: String? = null
+)
+
+data class SearchResourceParams(
+    val resourceType: String,
+    val identifier: String
+)
+
+data class SearchResourceResult(
+    val found: Boolean,
+    val resource: ResourceSummary? = null
+)
+
+data class RefreshResourcesParams(
+    val resources: List<ResourceRequest>
+)
+
+data class RefreshResourcesResult(
+    val resources: List<ResourceSummary>
+)
+
+data class ResourceStackManagementResult(
+    val isManaged: Boolean,
+    val stackName: String? = null,
+    val stackId: String? = null
+)
